955605365a96d402375174dd1e43d5cce298fef2
romagupalowich-dotcom <romagupalowich@gmail.com>
2026-08-16 19:52:14 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/gmtools/MainActivity.java
+++ app/src/main/java/com/example/gmtools/MainActivity.java
@@ -1,15 +1,46 @@
-package com.example.gmtools;
+package com.vuroxen.gmtools;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-        textView.setText("GMTools работает!");
-        setContentView(textView);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (id == R.id.nav_templates) {
+                selectedFragment = new TemplatesFragment();
+            } else if (id == R.id.nav_stats) {
+                selectedFragment = new StatsFragment();
+            } else if (id == R.id.nav_settings) {
+                selectedFragment = new SettingsFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+        });
+
+        // Загружаем HomeFragment по умолчанию
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        }
     }
-          }
+}
